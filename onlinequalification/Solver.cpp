676ddcc28d20c2116bbe6593282fea4b5b98f7ad
sbriss_a <sbriss_a@epitech.eu>
2016-02-11 22:22:48 +0100

    getScore semble marcher

--- onlinequalification/Solver.cpp
+++ onlinequalification/Solver.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <algorithm>
 #include <map>
+#include <iostream>
 #include "RoundIO.hpp"
 
 int distance(int r1, int c1, int r2, int c2) {
@@ -36,24 +37,24 @@ public:
 
     int r;
     int c;
-    Order order;
+  std::list<int> liste;
 
     bool check_availability(const Warehouse &a)
     {
       std::map<int, int> items;
 
-      for (int i = 0; i < order.nb_items; i++)
+      for (int i : liste)
       {
-        items[order.items[i]] = 0;
+        items[i] = 0;
       }
-      for (int i = 0; i < order.nb_items; i++)
+      for (int i : liste)
       {
-        items[order.items[i]]++;
+        items[i]++;
       }
 
-      for (int i = 0; i < order.nb_items; i++)
+      for (int i : liste)
       {
-        if (order.items[i] > a.nb_products[i])
+        if (items[i] > a.nb_products[i])
           return false;
       }
       return true;
@@ -69,67 +70,71 @@ public:
 };
 
 
-std::vector<Warehouse> ClosestWarehouses(Warehouse *warehouses, int nb_warehouses, int *r, int *c) {
+std::vector<Warehouse> ClosestWarehouses(Warehouse *warehouses, int nb_warehouses, int r, int c) {
   std::vector<Warehouse> whs;
 
   for (int i = 0; i < nb_warehouses; ++i) {
     whs.push_back(warehouses[i]);
   }
   compare comp;
-  comp.r = *r;
-  comp.c = *c;
+  comp.r = r;
+  comp.c = c;
   std::sort(whs.begin(), whs.end(), comp);
   return whs;
 }
 
-std::vector<Warehouse> BestWarehouses(Order order, Warehouse *warehouses, int nb_warehouses, int *r, int *c) {
+std::vector<Warehouse> BestWarehouses(std::list<int> items, Warehouse *warehouses, int nb_warehouses, int r, int c) {
   std::vector<Warehouse> whs;
 
   for (int i = 0; i < nb_warehouses; ++i) {
     whs.push_back(warehouses[i]);
   }
   compare_best comp;
-  comp.r = *r;
-  comp.c = *c;
-  comp.order = order;
+  comp.r = r;
+  comp.c = c;
+  comp.liste = items;
   std::sort(whs.begin(), whs.end(), comp);
   return whs;
 }
 
 
-Warehouse *gotoClosestWarehouse(Warehouse *warehouses, int nb_warehouses, int *r, int *c, int *time) {
-  Warehouse *w = warehouses;
-  int pdist = distance(*r, *c, w->row, w->column);
+// Warehouse *gotoClosestWarehouse(Warehouse *warehouses, int nb_warehouses, int *r, int *c, int *time) {
+//   Warehouse *w = warehouses;
+//   int pdist = distance(*r, *c, w->row, w->column);
 
-  for (int i = 1; i < nb_warehouses; ++i) {
-    int dist = distance(*r, *c, warehouses[i].row, warehouses[i].column);
-    if (dist < pdist) {
-      w = &warehouses[i];
-      pdist = dist;
-    }
-  }
-  *time += pdist;
-  *r = w->row;
-  *c = w->column;
-  return w;
-}
+//   for (int i = 1; i < nb_warehouses; ++i) {
+//     int dist = distance(*r, *c, warehouses[i].row, warehouses[i].column);
+//     if (dist < pdist) {
+//       w = &warehouses[i];
+//       pdist = dist;
+//     }
+//   }
+//   *time += pdist;
+//   *r = w->row;
+//   *c = w->column;
+//   return w;
+// }
 
-int getScore(Drone *drone, Order *order, Warehouse *warehouses, int nb_warehouses, Product *products, int nb_products) {
+int getScore(Drone *drone, Order *order) {
   int time = 0;
   int drone_r = drone->row;
   int drone_c = drone->column;
-  Warehouse *hand;
+  std::vector<Warehouse> bestWH;
   std::list<int> items;
   for (int i = 0; i < order->nb_items; ++i) {
     items.push_front(order->items[i]);
   }
 
   while (!items.empty()) {
-    hand = gotoClosestWarehouse(warehouses, nb_warehouses, &drone_r, &drone_c, &time);
-    // fill drone (items, hand, &time)
-    time += distance(drone_r, drone_c, order->row, order->column);
-    // empty drone
-  }
+    std::cout << items.size() << std::endl;
+    //hand = gotoClosestWarehouse(warehouses, nb_warehouses, &drone_r, &drone_c, &time);
+    bestWH = BestWarehouses(items, round_io.warehouses, round_io.nb_warehouses, drone_r, drone_c);
+    time += distance(drone_r, drone_c, bestWH[0].row, bestWH[0].column); // goto best WH
+    time += 2 * bestWH[0].search_items(items, round_io.nb_max_payload, round_io.products); // 2* car il faut autant de temps pour les prendre que pour les poser
+    time += distance(bestWH[0].row, bestWH[0].column, order->row, order->column); // goto order spot
+    drone_c = order->column;
+    drone_r = order->row;
+      }
   return time;
   // get closest warehouse
   // fill drone to max
