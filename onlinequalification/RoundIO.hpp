caef4db0ddb51cda959c24d1fff937207f430dd5
Maigret Aurélien <dewep.net@gmail.com>
2016-02-11 18:21:58 +0000

    Base parsing

--- onlinequalification/RoundIO.hpp
+++ onlinequalification/RoundIO.hpp
@@ -6,34 +6,82 @@
 #include <string>
 #include <sstream>
 
+struct Product {
+    int id;
+    int weight;
+};
+
+struct Warehouse {
+    int row;
+    int column;
+    int *nb_products;
+};
+
+struct Order {
+    int row;
+    int column;
+    int nb_items;
+    int *items;
+};
+
 struct RoundIO {
-    int rows;
-    int columns;
-    char **cells;
+    int nb_rows;
+    int nb_columns;
+    int nb_drones;
+    int nb_turns;
+    int nb_max_payload;
+    int nb_products;
+    int nb_warehouses;
+    int nb_orders;
+    Product *products;
+    Warehouse *warehouses;
+    Order *orders;
 
     RoundIO() {
-        std::cin >> rows;
-        std::cin >> columns;
-        cells = new char*[rows];
-        for (int r = 0; r < rows; r++) {
-            cells[r] = new char[columns];
-            for (int c = 0; c < columns; c++) {
-                std::cin >> cells[r][c];
+        std::cin >> nb_rows;
+        std::cin >> nb_columns;
+        std::cin >> nb_drones;
+        std::cin >> nb_turns;
+        std::cin >> nb_max_payload;
+        std::cin >> nb_products;
+        products = new Product[nb_products];
+        for (int p = 0; p < nb_products; p++) {
+            products[p].id = p;
+            std::cin >> products[p].weight;
+        }
+        std::cin >> nb_warehouses;
+        warehouses = new Warehouse[nb_warehouses];
+        for (int w = 0; w < nb_products; w++) {
+            std::cin >> warehouses[w].row;
+            std::cin >> warehouses[w].column;
+            warehouses[w].nb_products = new int[nb_products];
+            for (int wp = 0; wp < nb_products; wp++) {
+                std::cin >> warehouses[w].nb_products[wp];
+            }
+        }
+        std::cin >> nb_orders;
+        orders = new Order[nb_orders];
+        for (int o = 0; o < nb_orders; o++) {
+            std::cin >> orders[o].row;
+            std::cin >> orders[o].column;
+            std::cin >> orders[o].nb_items;
+            for (int oi = 0; oi < orders[o].nb_items; oi++) {
+                std::cin >> orders[o].items[oi];
             }
         }
     }
 
-    std::list<std::string> commands;
+    /*std::list<std::string> commands;
     void addXXXXXXXXXX(int a, int b, int c) {
         std::ostringstream os;
         os << "PAINT_SQUARE " << a << " " << b << " " << c;
         commands.push_back(os.str());
-    }
+    }*/
     int writeOutput() {
-        std::cout << commands.size() << std::endl;
+        /*std::cout << commands.size() << std::endl;
         for (auto it = commands.begin(); it != commands.end(); ++it) {
             std::cout << *it << std::endl;
-        }
+        }*/
         return 0;
     }
 };
--- onlinequalification/main.cpp
+++ onlinequalification/main.cpp
@@ -6,10 +6,7 @@ int main() {
     std::ios::sync_with_stdio(false);
     RoundIO round_io = RoundIO();
 
-    std::cerr << "getRows: " << round_io.rows << std::endl;
-    std::cerr << "getColumns: " << round_io.columns << std::endl;
-    std::cerr << "Second row: " << round_io.cells[1] << std::endl;
-    std::cerr << "Cell [3;3]: " << round_io.cells[3][3] << std::endl;
+    std::cerr << "nb_rows: " << round_io.nb_rows << std::endl;
 
     //round_io.addXXXXXXXXXX(1, 2, 3);
 
